25a58f89d0e51679ffb67258cbb91af50c501319
Carson Hoffman <c@rsonhoffman.com>
2020-10-09 21:54:59 -0400

    Fix notifications on Safari

--- frontend/src/util/Notification.ts
+++ frontend/src/util/Notification.ts
@@ -1,7 +1,25 @@
+function checkNotificationPromise() {
+	try {
+		Notification.requestPermission().then();
+	} catch (e) {
+		return false;
+	}
+
+	return true;
+}
+
 export default function SendNotification(title: string, body: string) {
-	Notification.requestPermission().then((p) => {
-		if (p === 'granted') {
-			new Notification(title, {body: body});
-		}
-	})
+	if (checkNotificationPromise()) {
+		Notification.requestPermission().then((p) => {
+			if (p === 'granted') {
+				new Notification(title, {body: body});
+			}
+		});
+	} else {
+		Notification.requestPermission((p) => {
+			if (p === 'granted') {
+				new Notification(title, {body: body});
+			}
+		});
+	}
 }
