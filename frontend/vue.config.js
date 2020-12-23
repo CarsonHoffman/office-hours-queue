d48808bb0d54dfec41ec3e0f6992a28a22ed6baa
Carson Hoffman <c@rsonhoffman.com>
2020-12-22 22:49:49 -0500

    Only attempt to set devServer when in dev

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,20 +7,24 @@ module.exports = {
 			title: 'EECS Office Hours',
 		},
 	},
-	devServer: {
-		proxy: {
-			'^/api': {
-				target: 'https://lvh.me:8080',
-				ws: true,
-				// changeOrigin didn't work for WebSocket connections.
-				onProxyReqWs: function(request) {
-					request.setHeader('origin', 'https://lvh.me:8080');
+	configureWebpack: (config) => {
+		if (process.env.NODE_ENV !== 'production') {
+			config.devServer = {
+				proxy: {
+					'^/api': {
+						target: 'https://lvh.me:8080',
+						ws: true,
+						// changeOrigin didn't work for WebSocket connections.
+						onProxyReqWs: function(request) {
+							request.setHeader('origin', 'https://lvh.me:8080');
+						},
+					},
 				},
-			},
-		},
-		https: true,
-		key: fs.readFileSync('../deploy/secrets/certs/localhost-key.pem'),
-		cert: fs.readFileSync('../deploy/secrets/certs/localhost.pem'),
-		transportMode: 'ws',
+				https: true,
+				key: fs.readFileSync('../deploy/secrets/certs/localhost-key.pem'),
+				cert: fs.readFileSync('../deploy/secrets/certs/localhost.pem'),
+				transportMode: 'ws',
+			};
+		}
 	},
 };
