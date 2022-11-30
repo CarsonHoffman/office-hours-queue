1301e7f07b0d8063db99bdb8f90a3297662af61a
Carson Hoffman <c@rsonhoffman.com>
2022-11-30 18:28:43 -0500

    Yes I want to sanitize

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'@typescript-eslint/camelcase': 'off',
+		'no-control-regex': 0,
 	},
 };
